bf7d3accde93b17872c008270f6d6d3ecce7896e
rohithreddy1095 <rohithreddy1095@gmail.com>
2023-12-03 16:20:49 +0530

    Refactor read_tree function and add _empty_current_directory function

--- ugit/base.py
+++ ugit/base.py
@@ -27,6 +27,7 @@ def is_ignored(path):
     return '.ugit' in path.split('/')
 
 def read_tree (tree_oid):
+    _empty_current_directory ()
     for path, oid in get_tree (tree_oid, base_path='./').items ():
         os.makedirs (os.path.dirname (path), exist_ok=True)
         with open (path, 'wb') as f:
@@ -53,3 +54,21 @@ def _iter_tree_entries (oid):
     for entry in tree.decode ().splitlines ():
         type_, oid, name = entry.split (' ', 2)
         yield type_, oid, name
+
+def _empty_current_directory ():
+    for root, dirnames, filenames in os.walk ('.', topdown=False):
+        for filename in filenames:
+            path = os.path.relpath (f'{root}/{filename}')
+            if is_ignored (path) or not os.path.isfile (path):
+                continue
+            os.remove (path)
+        for dirname in dirnames:
+            path = os.path.relpath (f'{root}/{dirname}')
+            if is_ignored (path):
+                continue
+            try:
+                os.rmdir (path)
+            except (FileNotFoundError, OSError):
+                # Deletion might fail if the directory contains ignored files,
+                # so it's OK
+                pass
