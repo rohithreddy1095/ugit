ae2cd1b73b9d94e1b2392e9f45fe27f172f66ba5
rohithreddy1095 <rohithreddy1095@gmail.com>
2023-12-03 15:55:17 +0530

    Add read-tree function to base.py

--- ugit/cli.py
+++ ugit/cli.py
@@ -35,6 +35,10 @@ def parse_args():
     write_tree_parser = commands.add_parser("write-tree")
     write_tree_parser.set_defaults(func=write_tree)
 
+    read_tree_parser = commands.add_parser("read-tree")
+    read_tree_parser.set_defaults(func=read_tree)
+    read_tree_parser.add_argument("tree", help="The tree to read")
+
     return parser.parse_args()
 
 def init(args):
@@ -51,3 +55,6 @@ def cat_file(args):
 
 def write_tree(args):
     print(base.write_tree())
+
+def read_tree(args):
+    base.read_tree(args.tree)
